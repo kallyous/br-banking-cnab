2d25907f7202d13b1297af03e6bf1fd17242d097
Lucas <lcf@ic.ufal.br>
2021-08-17 01:04:22 -0300

    Ajusta nome de parâmetro.

--- brbankingcnab/__init__.py
+++ brbankingcnab/__init__.py
@@ -33,8 +33,8 @@ class CNABInvalidValueError(CNABError):
 class CNABInvalidOperationError(CNABError):
     """Exceção lançada quando uma operação não suportada por certa classe é chamada."""
 
-    def __init__(self, class_name, method_name, message='\n\tA classe {0} não suporta o método {1}. {2}'):
-        super().__init__(message.format(class_name, method_name, message))
+    def __init__(self, class_name, method_name, extra='', message='\n\tA classe {0} não suporta o método {1}. {2}'):
+        super().__init__(message.format(class_name, method_name, extra))
 
 
 class CNABInvalidTemplateError(CNABError):
